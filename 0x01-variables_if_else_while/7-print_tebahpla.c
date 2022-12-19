139f7258cc50ee45a95d0e018821a3f673c1926e
Habtam12 <habtamwube@gmail.com>
2022-12-19 06:13:03 -0800

    print tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the entry
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	char nl;
+
+	c = 'z';
+	nl = '\n';
+	while (c >= 'a')
+		putchar(c--);
+	putchar(nl);
+	return (0);
+}
+Footer
+
