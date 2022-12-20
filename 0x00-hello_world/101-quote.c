14675e9032304bd8549e067b1544fee9a903b2e5
Habtam12 <habtamwube@gmail.com>
2022-12-19 22:27:08 -0800

    quote file added.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - The main function
+ * Return: 1 for error
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - ");
+	fprintf(stderr, "Dora Korpar, 2015-10-19\n");
+	return (1);
+}
