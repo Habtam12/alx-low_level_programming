36ffedab8c7211fa2e4d3c351a3974024577e666
Habtam12 <habtamwube@gmail.com>
2022-12-19 22:07:26 -0800

    print the text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - The main function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
