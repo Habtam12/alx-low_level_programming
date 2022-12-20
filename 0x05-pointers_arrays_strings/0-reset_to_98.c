f5580806419a27d6198e2b884fd6d61289f31c7c
Habtam12 <habtamwube@gmail.com>
2022-12-20 03:47:27 -0800

    Readme updated.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to a parameter and updates its value.
- * @n: input integer.
- * Return: no return.
+ * reset_to_98 - resets the arguments value of the pointer to 98
+ * @n: pointer to integer
+ * Return: void
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers.
- * @a: first integer.
- * @b: second integer.
- * Return: no return.
+ * swap_int - swaps the values of two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ * Return: void
  */
 void swap_int(int *a, int *b)
 {
-	int c = *a;
+	int tmp;
+
+	tmp = *a;
 	*a = *b;
-	*b = c;
+	*b = tmp;
 }
