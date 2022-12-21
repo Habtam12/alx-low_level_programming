5d24776daac2cc87d05892ecaec849481e524b17
Habtam12 <habtamwube@gmail.com>
2022-12-20 23:26:13 -0800

    main and readme added.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - function that checks for uppercase characters
- * @c: int type number
- * Return: 1 if uppercase, else 0
+ * _isupper - checks for case
+ * @c: the character
+ * Return: 1 for upper anad 0 for lower
  */
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
+	if (c < 65 || c > 90)
+		return (0);
+	else
 		return (1);
-
 	return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,13 @@
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);i
+
+void print_number(int n);
