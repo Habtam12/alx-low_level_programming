4e3968eb0769b9e12df9e41fb0589e067f53cfec
Habtam12 <habtamwube@gmail.com>
2022-12-21 00:01:22 -0800

    10 times the number.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,27 @@
 #include "main.h"
 
 /**
- * print_line - prints line with the length proportional to n
- * @n: the length of the line; one unit = one underscore ('_')
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  * Return: void
  */
-void print_line(int n)
+void more_numbers(void)
 {
 	int i;
+	int j;
+	int j_char_left;
+	int j_char_right;
 
-	if (n > 0)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 1; i <= n; i++)
-			_putchar('_');
+		for (j = 0; j < 15; j++)
+		{
+			j_char_left = j / 10;
+			j_char_right = j % 10;
+
+			if (j >= 10)
+				_putchar(j_char_left + 48);
+			_putchar(j_char_right + 48);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
