a503c757bae602d7d2aee2b5e3300a8f105e924f
Habtam12 <habtamwube@gmail.com>
2022-12-21 18:24:33 -0800

    main updated

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,27 @@
-void _putchar(char c);
+#ifndef MAIN_H
+
+#define MAIN_H
+
 char *_strcat(char *dest, char *src);
+
 char *_strncat(char *dest, char *src, int n);
+
 char *_strncpy(char *dest, char *src, int n);
+
 int _strcmp(char *s1, char *s2);
+
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+
+char *string_toupper(char *str);
+
+char *cap_string(char *str);
+
+char *leet(char *str);
+
+char *rot13(char *str);
+
+void print_number(int n);
+
+int _putchar(char c);
+
+#endifi
