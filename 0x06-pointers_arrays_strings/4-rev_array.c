bc29bae2f6c80faa23c514532ee3e853726df09c
Habtam12 <habtamwube@gmail.com>
2022-12-22 05:09:15 -0800

    task four updated

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,6 @@
 /**
  *reverse_array - reverses array of integers
  *@a: input array
- 
  *@n: length of array
  *Return: void
  */
@@ -13,11 +12,7 @@ void reverse_array(int *a, int n)
 {
 
 		int j, temp, i = n;
-
-
-
-			for (i--, j = 0; j < n / 2; j++, i--)
-
-						temp = a[j], a[j] = a[i], a[i] = temp;
+		for (i--, j = 0; j < n / 2; j++, i--)
+			temp = a[j], a[j] = a[i], a[i] = temp;
 
 }
