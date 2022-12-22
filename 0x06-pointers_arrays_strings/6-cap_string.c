1a1a945466198f030df9a6f08821f97397dc24a0
Habtam12 <habtamwube@gmail.com>
2022-12-22 05:53:06 -0800

    task 6 updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,35 +7,25 @@
 
 
 /**
-
- * cap_string - capitalizes all words of a string.
-
- * @str: pointer for the string
-
- * Return: pointer for the capitalized string.
-
- */
+ 
+  * cap_string - capitalizes all words of a string.
+ 
+  * @str: pointer for the string
+ 
+  * Return: pointer for the capitalized string.
+ 
+  */
 
 char *cap_string(char *str)
 
 {
-
-		int i;
-
-			int j;
-
-				char sep[] = " \t\n,;.!?\"(){}";
-
-
-
-					printf("%lu\n", sizeof(sep));
-
-						for (i = 0; *(str + i) != '\0'; i++)
-
-									for (j = 0; sep[j] != '\0'; j++)
-
-													;
-
-							return (str);
+	int i;
+	int j;
+	char sep[] = " \t\n,;.!?\"(){}";
+	printf("%lu\n", sizeof(sep));
+	for (i = 0; *(str + i) != '\0'; i++)
+		for (j = 0; sep[j] != '\0'; j++)
+			;
+	return (str);
 
 }
