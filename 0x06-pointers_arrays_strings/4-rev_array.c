1b7a3685477bb1f0a4c32af67624a736e7441d69
Habtam12 <habtamwube@gmail.com>
2022-12-22 05:17:10 -0800

    task four updated

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,14 +3,10 @@
 
 
 /**
- *
- *  * reverse_array - Reverses the content of an array of integers.
- *
- *   * @a: The array of integers to be reversed.
- *
- *    * @n: The number of elements in the array.
- *
- *     */
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
+ */
 
 void reverse_array(int *a, int n)
 
