7fcd05fb8fe26a378754b6301fad8e37ece26e01
Habtam12 <habtamwube@gmail.com>
2022-12-21 18:54:21 -0800

    fixed

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,19 +1,17 @@
-int _putchar(char c);
+void _putchar(char c);
 
-char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
 
-char *_strchr(char *s, char c);
+char *_strncpy(char *dest, char *src, int n);
 
-unsigned int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
 
-char *_strpbrk(char *s, char *accept);
+void reverse_array(int *a, int n);
 
-char *_strstr(char *haystack, char *needle);
+char *string_toupper(char *);
 
-void print_chessboard(char (*a)[8]);
+char *cap_string(char *);
 
-void print_diagsums(int *a, int size);
-
-void set_string(char **s, char *to);
+char *leet(char *);
