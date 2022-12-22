1413aca37f891d0fbc0011d75f1f1caedb7cfa5c
Habtam12 <habtamwube@gmail.com>
2022-12-22 03:44:26 -0800

    string concatination updated.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,61 @@
 #include "main.h"
 
-#include <string.h>
-
-
-
 /**
  *
  *  * _strncat - concatenates two strings
  *
- *   * @dest: pointer for the destination string
+ *   * @dest: input parameter string
  *
- *    * @src: pointer for the source string
+ *    * @src: input parameter string
  *
- *     * @n: number of bytes from the src string
+ *     * @n: times to iterations
  *
- *      * Return: pointer for the concatenated string
+ *      *
  *
- *       */
+ *       * Return: dest
+ *
+ *        */
 
 char *_strncat(char *dest, char *src, int n)
 
 {
 
-		return (strncat(dest, src, n));
+		int a;
+
+			int b;
+
+
+
+				a = 0;
+
+
+
+					while (dest[a] != 0)
+
+							{
+
+										a++;
+
+											}
+
+
+
+						b = 0;
+
+
+
+							while (src[b] != 0 && b < n)
+
+									{
+
+												dest[a] = src[b];
+
+														a++;
+
+																b++;
+
+																	}
+
+								return (dest);
 
 }
