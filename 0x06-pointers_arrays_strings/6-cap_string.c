07873bbca54e9d70db9a1e4b4b7d7b929e0d9948
Habtam12 <habtamwube@gmail.com>
2022-12-22 17:53:39 -0800

    added

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,26 +15,54 @@
 
   */
 
-char *cap_string(char *str)
+char *cap_string(char *entry)
 
 {
 
-		int i;
-
-			int j;
-
-				char sep[] = " \t\n,;.!?\"(){}";
+		int conversion, index, count;
 
 
 
-					printf("%lu\n", sizeof(sep));
+			char chars[] = {' ', ',', ';', '.', '!',
 
-						for (i = 0; *(str + i) != '\0'; i++)
+							 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
 
-									for (j = 0; sep[j] != '\0'; j++)
+				conversion = 32;
 
-													;
 
-							return (str);
+
+					for (index = 0; entry[index] != '\0'; index++)
+
+							{
+
+										if (entry[index] >= 'index' && entry[index] <= 'z')
+
+													{
+
+																	entry[index] =  entry[index] - conversion;
+
+																			}
+
+												conversion = 0;
+
+														for (count = 0; chars[count] != '\0'; count++)
+
+																	{
+
+																					if (chars[count] == entry[index])
+
+																									{
+
+																														conversion = 32;
+
+																																		break;
+
+																																					}
+
+																							}
+
+															}
+
+						return (entry);
 
 }
