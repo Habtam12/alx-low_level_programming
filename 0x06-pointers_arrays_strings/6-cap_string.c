4fd75bdd43139324906c8f1776099690dbbc833b
Habtam12 <habtamwube@gmail.com>
2022-12-22 17:49:01 -0800

    added

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,35 +4,37 @@
 
 #include <stdio.h>
 
+
+
 /**
-* cap_string - capitalizes all words of a string
-*  @str: the string to change the first letter of a word in uppercase
-*  Return: capitalizes letters
-*/
+ 
+  * cap_string - capitalizes all words of a string.
+ 
+  * @str: pointer for the string
+  * Return: pointer for the capitalized string.
+
+  */
 
 char *cap_string(char *str)
 
 {
-	int i;
-	int j;
-	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (i == 0 && str[i] >= 97 && str[i] <= 122)
-		{
-			str[i] = str[i] - 32;
-		}
-		j = 0;
-		while (c[j] != '\0')
-		{
-			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
-			j++;
-		}
-		i++;
-	}
-	return (str);
+
+		int i;
+
+			int j;
+
+				char sep[] = " \t\n,;.!?\"(){}";
+
+
+
+					printf("%lu\n", sizeof(sep));
+
+						for (i = 0; *(str + i) != '\0'; i++)
+
+									for (j = 0; sep[j] != '\0'; j++)
+
+													;
+
+							return (str);
+
 }
