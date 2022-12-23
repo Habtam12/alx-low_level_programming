5d67a9ea3ff9d9977c1c6cb0c8dddb3257d889ab
Habtam12 <habtamwube@gmail.com>
2022-12-23 04:09:38 -0800

    leet updated.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,13 +5,10 @@
 
 
 /**
-  * leet - encodes a string into 1337.
-
+ * leet - encodes a string into 
  * @s: pointer for the string
- 
  * Return: pointer for the encoded string
-
-  */
+ */
 
 char *leet(char *str)
 
