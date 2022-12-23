0288b736285fe34cadf6b074ece22d0e3bff8093
Habtam12 <habtamwube@gmail.com>
2022-12-22 23:57:56 -0800

    four fixed

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,11 +14,15 @@
 void reverse_array(int *a, int n)
 
 {
-	int i;
-	int array[1000];
-	for (i = 0; i < n; i++)
-		array[i] = *(a + n - 1 - i);
-	for (i = 0; i < n; i++)
-		*(a + i) = array[i];
+	int index, placeholder;
+	placeholder = 0;
+	n--;
+	for (index = 0; index < n; index++)
+	{
+		placeholder = a[n];
+		a[n] = a[index];							
+		a[index] = placeholder;
+		n--;
+	}
 
 }
