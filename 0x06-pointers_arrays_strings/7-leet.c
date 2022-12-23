e7a1b91ac873decd0a8800a7a3ac1737d251870a
Habtam12 <habtamwube@gmail.com>
2022-12-23 04:03:51 -0800

    compose

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,10 +13,25 @@
 
   */
 
-char *leet(char *s)
+char *leet(char *str)
 
 {
-
-		return (s);
-
+	int i, j;
+	char c[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (c[j] != '\0')
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = n[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
 }
