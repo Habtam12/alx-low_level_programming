414df675a3be6c859a6f5024e67ba5900dc8f75d
Habtam12 <habtamwube@gmail.com>
2022-12-23 04:18:32 -0800

    leet updated.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,26 +9,20 @@
   * @s: pointer for the string
   * Return: pointer for the encoded string
   */
-
 char *leet(char *str)
 
 {
-	int i, j;
-	char c[] = "aAeEoOtTlL";
-	char n[] = "4433007711";
-	i = 0;
-	while (str[i] != '\0')
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	while (str[indx1])
 	{
-		j = 0;
-		while (c[j] != '\0')
+		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-			if (str[i] == c[j])
-			{
-				str[i] = n[j];
-			}
-			j++;
+			if (str[indx1] == leet[indx2] ||	
+	    				str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
 		}
-		i++;
+		indx1++;
 	}
 	return (str);
 }
