022448ba4c003430d277a705d3765dd4353f1c23
Habtam12 <habtamwube@gmail.com>
2023-01-03 04:34:52 -0800

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,60 +1,83 @@
 #include "main.h"
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 /**
-  * infinite_add - add two numbers
-  * @n1: first number
-  * @n2: second number
-  * @r: the buffer
-  * @size_r: the size of r
-  * Return: char
-  */
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: The string containing the first number to be added.
+ * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result.
+ * @r_index: The current index of the buffer.
+ *
+ * Return: If r can store the sum - a pointer to the result.
+ *         If r cannot store the sum - 0.
+ */
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+
+	else if (tens && r_index < 0)
+		return (0);
+
+	return (r + r_index + 1);
+}
+/**
+ * infinite_add - Adds two numbers.
+ * @n1: The first number to be added.
+ * @n2: The second number to be added.
+ * @r: The buffer to store the result.
+ * @size_r: The buffer size.
+ *
+ * Return: If r can store the sum - a pointer to the result.
+ *         If r cannot store the sum - 0.
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int index, n1_len = 0, n2_len = 0;
 
-	int i = 0;
-	int j = 0;
-	int count = 0;
-	int digit = 0;
-	unsigned int digitval;
-	unsigned int value1 = 0;
-	unsigned int value2 = 0;
-	unsigned int value3 = 0;
-	char *p = r;
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
 
-	while (n1[i + 1])
-	{
-		n1++;
-		i++;
-	}
-	while (i)
-	{
-		value1 += n1[i] - '0';
-		i--;
-	}
-	while (n2[j + 1])
-	{
-		n2++;
-		j++;
-	}
-	while (j)
-	{
-		value2 += n2[j - '0'];
-		j--;
-	}
-	value3 = value1 + value2;
-	digitval = value3;
-	while (digitval / 10)
-	{
-		count += 1;
-		digitval /= 10;
-	}
-	count += 1;
-	if (count > size_r)
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
-	for (; count; count--)
-	{
-		digit = (value3 % 10);
-		value3 = (value3 - digit);
-		r[count] = digit + '0';
-	}
-	return (p);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
