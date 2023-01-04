3f592ec5eacafb535b5b3a1d0e1e281cf4d24bf1
Habtam12 <habtamwube@gmail.com>
2023-01-03 23:20:24 -0800

    updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,21 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
- * ahhh! Crazy task! Expand your knowledge
- * Return: address of r or 0
+ * infinite_add - add two string numbers
+ * @n1: string number to add to n2
+ * @n2: string number to add to n1
+ * @r: buffer to store the sum
+ * @size_r: size of buffer
+ * return: pointer to sum or 0 if it does not fit in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
-
-	for (i = 0; n1[i]; i++)
+	int len1 = 0, len2 = 0, lenS = 0;
+	
+	while (*(n1 + len1++))
 		;
-	for (j = 0; n2[j]; j++)
+	while (*(n2 + len2++))
 		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
-	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
-	}
+	printf("%d, %d\n", len1, len2);
 	return (r);
 }
