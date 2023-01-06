f0b9d88810196c2ac4d556066a8f0454db54a1a4
Habtam12 <habtamwube@gmail.com>
2023-01-06 03:24:23 -0800

    prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - the main function
- * @argc: arguments count
- * @argv: arguments vector
- * Return: 0 for success or 1 for error
- */
+* main - prints the minimum number of coins to make change for a given amount
+* @argc: arguement count
+* @argv: array of pointers to arguement strings
+* Return: number of coins or 1
+**/
 int main(int argc, char *argv[])
 {
-	int i;
-	int x;
-	int change;
-	int y[5] = {25, 10, 5, 2, 1};
+	int amount, coins;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	x = atoi(argv[1]);
-	if (x < 0)
+	amount = atoi(argv[1]);
+	coins = 0;
+	if (amount > 25)
 	{
-		printf("0\n");
-		return (0);
+		while (amount >= 25)
+			amount -= 25, coins++;
 	}
-
-	change = 0;
-	while (x > 0)
+	if (amount > 10 && amount < 25)
 	{
-		for (i = 0; i < 5; i++)
-			if (x >= y[i] && x > 0)
-			{
-				x -= y[i];
-				change++;
-			}
+		while (amount >= 10)
+			amount -= 10, coins++;
 	}
-	printf("%d\n", change);
+	if (amount > 5 && amount < 10)
+	{
+		while (amount >= 5)
+			amount -= 5, coins++;
+	}
+	if (amount > 2 && amount < 5)
+	{
+		while (amount >= 2)
+			amount -= 2, coins++;
+	}
+	if (amount == 1 || amount == 2 || amount == 5 ||
+	    amount == 10 || amount == 25)
+	{
+		coins++;
+	}
+	printf("%d\n", coins);
 	return (0);
 }
