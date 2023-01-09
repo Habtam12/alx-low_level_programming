eccfc55fe5371f8940f968b6045de5638cdd0b66
Habtam12 <habtamwube@gmail.com>
2023-01-08 22:07:47 -0800

    a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,64 +1,44 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
- * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
+ * argstostr - concatenates all the arguments
+ * @ac: arguments count
+ * @av: arguments vector
+ * Return: pointer for the concatenated str
  */
-char **strtow(char *str)
+char *argstostr(int ac, char **av)
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+	int i;
+	int j;
+	int col;
+	int size;
+	char *str;
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
-	if (*str == '\0' || str == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	size = 0;
+	for (i = 0; i < ac; i++)
+		size += strlen(av[i]) + 1;
+	str = malloc(size * sizeof(char));
+	if (str)
 	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
-	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == NULL)
-		return (NULL);
-	for (wordf = 0; str[wordf] && j <= w; wordf++)
-	{
-		count = 0;
-		if (str[wordf] != ' ')
+		for (i = 0; i < ac; i++)
 		{
-			for (i = wordf ; str[i] != '\0'; i++)
+			col = (strlen(av[i]) + 1) * sizeof(char);
+			j = 0;
+			while (av[i][j] != '\0')
 			{
-				if (str[i] == ' ')
-					break;
-				count++;
+				str[i * col + j] = av[i][j];
+				j++;
 			}
-			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + j) == NULL)
-			{
-				for (k = 0; k <= j; k++)
-				{
-					x = p[k];
-					free(x);
-				}
-				free(p);
-				return (NULL);
-			}
-			for (m = 0; wordf < i; wordf++)
-			{
-				p[j][m] = str[wordf];
-				m++;
-			}
-			p[j][m] = '\0';
-			j++;
+			str[i * col + j] = ' ';
 		}
+		str[i * col + j] = '\n';
+		return (str);
 	}
-	p[j] = NULL;
-	return (p);
+	else
+		return (NULL);
 }
