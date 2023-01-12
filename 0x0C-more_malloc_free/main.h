05c1098c2b15a6b71af5a934faff9e68f0fd01c8
Habtam12 <habtamwube@gmail.com>
2023-01-12 01:18:23 -0800

    a function that allocates memory using malloc.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,22 +3,7 @@
 #define MAIN_H
 
 
-
-/**
- 
- * File: holberton.h
- 
- * Auth: Brennan D Baraban
- 
- * Desc: Header file containing prototypes for all functions
- 
- *used in the 0x0B-more_malloc_free directory.
- 
- */
-
-
-
-int _putchar(char c);
+putchar(char c);
 
 void *malloc_checked(unsigned int b);
 
