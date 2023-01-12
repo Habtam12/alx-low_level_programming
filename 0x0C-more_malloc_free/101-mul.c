d77fede893afbb2d6b1e28b6c8fff7577a8b8871
Habtam12 <habtamwube@gmail.com>
2023-01-11 22:55:58 -0800

    a program that multiplies two positive numbers.

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+
+
+/**
+ 
+ * main - the main function
+ 
+ * @argc: arguments count
+ 
+ * @argv: arguments vector
+ 
+ * Return: 0 for success
+ 
+ */
+
+int main(int argc, char *argv[])
+
+{
+	(void) argc;
+	(void) argv;
+	return (0);
+
+}
