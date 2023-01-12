af68b4b089db17c6011a6a855e678a61270728ed
Habtam12 <habtamwube@gmail.com>
2023-01-12 01:09:46 -0800

    main updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,25 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+/**
+ 
+ * File: holberton.h
+ 
+ * Auth: Brennan D Baraban
+ 
+ * Desc: Header file containing prototypes for all functions
+ 
+ *used in the 0x0B-more_malloc_free directory.
+ 
+ */
+
+
+
+int _putchar(char c);
+
 void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -8,4 +30,6 @@ int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-void _putchar(char c);
+
+
+#endif
