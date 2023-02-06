df65f0e4661e604b4b430e9b29fcde95159305cd
Habtam12 <habtamwube@gmail.com>
2023-02-06 00:11:29 -0800

    task 1 updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,68 +1,18 @@
 #include "main.h"
 
 /**
- * _pow - computes 2 the power of n
- * @n: the power
- * Return: the result
+ * print_binary - printg the binary represeantation of a number
+ * @n: a string of 0 and 1 chars
+ *
  */
-unsigned int _pow(unsigned int n)
-{
-	if (n <= 0)
-		return (1);
-	return (2 * _pow(n - 1));
-}
 
-/**
- * bit_index - returns the index of the most significant bit
- * @n: the nember for which the index is going to be found
- * Return: the index
- */
-unsigned int bit_index(unsigned long int n)
-{
-	unsigned int m;
-
-	m = 0;
-	if (n <= 1)
-		return (0);
-	while (_pow(m) <= n)
-		m++;
-	m--;
-	return (m);
-}
-
-/**
- * _print_bits - prints the bits recursively
- * @i: the bit shift counter
- * @m: the number of bits counter
- * @n: the original decimal number
- * Return: nothing
- */
-void _print_bits(int i, int m, unsigned long int n)
-{
-	if (m >= 0)
-	{
-		m--;
-		i++;
-
-		_print_bits(i, m, n);
-		_putchar((1 & (n >> i)) + '0');
-	}
-	else
-		return;
-}
-
-/**
- * print_binary - prints the binary representation of a number
- * @n: the number in base 10
- * Return: nothing
- */
 void print_binary(unsigned long int n)
 {
-	int m;
-	int i;
+	int printbin = ((n & 1) + '0');
 
-	m = bit_index(n);
-	i = -1;
-	_print_bits(i, m, n);
+	if (n > 1)
+	{
+		print_binary((n >> 1));
+	}
+	_putchar(printbin);
 }
-
